e1ebfba27e7b2b9d9ee87b9fd120e88fbb152cdd
Junior Oliveira <junior.getsoftware@gmail.com>
2018-06-04 02:17:36 -0300

    tipos básicos

---
+++ tipos-basicos.ts
@@ -0,0 +1,37 @@
+//BOOLEAN 
+let estaPago: Boolean = true;
+
+//NUMBER
+let idade: Number = 20;
+let valor: Number = 1.99;
+
+//STRING
+let nome: String = 'Typescript';
+
+//  template string
+console.log(`meu nome é ${nome} e tenho ${idade} anos`);
+
+//arrays
+let notas:Number[] = [8, 9, 9 ,8, 7];
+
+//TUPLAS
+let alunos: [string, number, string] = ['type', 9, 'Matemática'];
+
+//Enums
+enum Cor {Verde, Amarelo,Azul,Branco};
+let corFundo: Cor = Cor.Branco;
+
+//Any
+let temAlgumValor:any = 4;
+temAlgumValor = 'outro valor';
+temAlgumValor = true;
+
+//void
+function alerta(): void{
+
+}
+
+// null e undefined
+
+
+
