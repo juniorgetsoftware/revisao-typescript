de158bdcd36e806e80dcd7f502a32770242d0ba1
Junior Oliveira <junior.getsoftware@gmail.com>
2018-06-04 00:54:43 -0300

    primeiro código com typescript

---
+++ primeiro-codigo-typescript.js
@@ -0,0 +1,14 @@
+/* JAVASCRIPT */
+function dizerOla(nome) {
+    console.log('Olá, ' + nome);
+}
+dizerOla('Javascript');
+/* TYPESCRIPT */
+function dizerOla2(nome) {
+    console.log('Olá, ' + nome);
+}
+dizerOla2('Typescript');
+function interpolacao(nome) {
+    console.log("Ol\u00E1, \n    " + nome);
+}
+interpolacao('Interpolação');
---
+++ primeiro-codigo-typescript.ts
@@ -0,0 +1,20 @@
+/* JAVASCRIPT */
+function dizerOla(nome){
+    console.log('Olá, ' + nome);
+}
+dizerOla('Javascript');
+
+
+/* TYPESCRIPT */
+function dizerOla2(nome: String){
+    console.log('Olá, ' + nome);
+}
+dizerOla2('Typescript');
+
+
+function interpolacao(nome: String){
+    console.log(`Olá, 
+    ${nome}`)
+}
+
+interpolacao('Interpolação');
